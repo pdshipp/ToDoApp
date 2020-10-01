1b8a24fecf17a13b5f588fb820d24a8a70d3fca5
pdshipp <peterdshipp@gmail.com>
2020-10-01 10:10:20 +0100

    adding dummy data above todo list

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,20 @@
 import React, { Component } from "react";
 import Todos from "./Todos.js";
 import AddTodo from "./addTodo";
+import axios from "axios";
 
 class ToDoPage extends Component {
   state = {
-    todos: [
-      { id: 1, content: "Buy Bread" },
-      { id: 2, content: "Watch Footy" },
-    ],
+    todos: [],
   };
+  componentDidMount() {
+    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
+      console.log(res);
+      this.setState({
+        todos: res.data.slice(0, 10),
+      });
+    });
+  }
   addTodo = (todo) => {
     todo.id = Math.random();
     let todos = [...this.state.todos, todo];
@@ -22,10 +28,26 @@ class ToDoPage extends Component {
     this.setState({ todos: todos });
   };
   render() {
+    const { todos } = this.state;
+    const todoList = todos.length ? (
+      todos.map((todo) => {
+        return (
+          <div className="post card" key={todo.id}>
+            <div className="card-content">
+              <span className="card-title">{todo.title}</span>
+              <p>{todo.body}</p>
+            </div>
+          </div>
+        );
+      })
+    ) : (
+      <div className="center">No todos yet</div>
+    );
     return (
       <div className="todo-app container">
         <h1 className="center teal-text text-lighten-2">Todo's</h1>
-        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+        <h1>{todoList}</h1>
+        <Todos todos={this.state.todo} deleteTodo={this.deleteTodo} />
         <AddTodo addTodo={this.addTodo} />
       </div>
     );
